858b3e5f27090ef60e405774b57aadb18d61bd97
Matthew Beattie <91721899+MBeattie02@users.noreply.github.com>
2024-03-07 10:15:05 +0000

    Update QualityTwo.java

--- Quality/QualityTwo.java
+++ Quality/QualityTwo.java
@@ -1,5 +1,54 @@
 package Quality;
 
+import java.util.List;
+
 public class QualityTwo {
 
+    // Duplicate method issue: Only one of these method2() should be present
+    public void method2() {
+        int product = 1;
+        for (int i = 1; i <= 5; i++) {
+            product *= i;
+        }
+        System.out.println("Product: " + product);
+    }
+
+    // Duplicate of the above method
+    public void method2() {
+        int product = 1;
+        for (int i = 1; i <= 5; i++) {
+            product *= i;
+        }
+        System.out.println("Product: " + product);
+    }
+
+
+    // Refactor suggestion: Convert loops to stream operations
+    public void processNumbers(List<Integer> numbers) {
+        // This loop can be refactored to a stream operation
+        numbers.forEach(System.out::println);
+
+        // This loop also can be refactored to a stream operation
+        numbers.stream()
+               .filter(number -> number > 10)
+               .forEach(this::processNumber);
+
+        // This loop is more complex and might not be easily refactored
+        for (Integer number : numbers) {
+            if (number > 10) {
+                processNumber(number);
+            } else {
+                handleSmallNumber(number);
+            }
+        }
+    }
+
+    private void processNumber(Integer number) {
+        // Some processing on the number
+    }
+
+    private void handleSmallNumber(Integer number) {
+        // Handle numbers <= 10
+    }
 }
+
